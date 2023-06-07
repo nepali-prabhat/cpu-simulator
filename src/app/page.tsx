311a7447603248b76f6be24c24eb6beb66f79f1f
Pravat Pandey <pandeypravat@gmail.com>
2023-06-07 13:33:08 +0545

    implemented zoom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,71 +8,64 @@ type NormalizedZoomValue = number & { _brand: "normalizedZoom" };
 const ZOOM_STEP = 0.1;
 const MIN_ZOOM = 0.1;
 
-function drawGrids(
-    ctx: CanvasRenderingContext2D,
-    canvasProperties: CanvasProperties,
-    renderProperties: {
-        gridSpace?: number;
+// TODO: understand this code
+const getStateForZoom = (
+    {
+        viewportX,
+        viewportY,
+        nextZoom,
+    }: {
+        viewportX: number;
+        viewportY: number;
+        nextZoom: NormalizedZoomValue;
+    },
+    appState: CanvasProperties
+) => {
+    const appLayerX = viewportX;
+    const appLayerY = viewportY;
+
+    const currentZoom = appState.zoom;
+
+    // get original scroll position without zoom
+    const baseScrollX =
+        appState.scroll.x + (appLayerX - appLayerX / currentZoom);
+    const baseScrollY =
+        appState.scroll.y + (appLayerY - appLayerY / currentZoom);
+
+    // get scroll offsets for target zoom level
+    const zoomOffsetScrollX = -(appLayerX - appLayerX / nextZoom);
+    const zoomOffsetScrollY = -(appLayerY - appLayerY / nextZoom);
+
+    return {
+        scroll: {
+            x: baseScrollX + zoomOffsetScrollX,
+            y: baseScrollY + zoomOffsetScrollY,
+        },
+        zoom: nextZoom,
+    };
+};
+const strokeGrid = (
+    context: CanvasRenderingContext2D,
+    gridSize: number,
+    offsetX: number,
+    offsetY: number,
+    width: number,
+    height: number
+) => {
+    context.save();
+    context.strokeStyle = "rgba(0,0,0,0.1)";
+    context.beginPath();
+    for (let x = offsetX; x < offsetX + width + gridSize * 2; x += gridSize) {
+        context.moveTo(x, offsetY - gridSize);
+        context.lineTo(x, offsetY + height + gridSize * 2);
     }
-) {
-    const { zoom, origin, width, height } = canvasProperties;
-    const { gridSpace = 10 } = renderProperties;
-
-    ctx.save();
-    // translate to nearest 10th place
-    let translateX = -origin.x;
-    let offsetX = 0;
-    if (translateX < 0) {
-        offsetX = Math.abs(translateX % gridSpace);
-        translateX = translateX + offsetX;
-    } else {
-        offsetX = gridSpace - Math.abs(translateX % gridSpace);
-        translateX = translateX + offsetX;
+    for (let y = offsetY; y < offsetY + height + gridSize * 2; y += gridSize) {
+        context.moveTo(offsetX - gridSize, y);
+        context.lineTo(offsetX + width + gridSize * 2, y);
     }
-
-    let translateY = -origin.y;
-    let offsetY = 0;
-    if (translateY < 0) {
-        offsetY = Math.abs(translateY % gridSpace);
-        translateY = translateY + offsetY;
-    } else {
-        offsetY = gridSpace - Math.abs(translateY % gridSpace);
-        translateY = translateY + offsetY;
-    }
-
-    ctx.translate(translateX, translateY);
-
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(translateX, translateY, width + offsetX, height + offsetY);
-
-    ctx.strokeStyle = "hsla(66, 40%, 90%, 1)";
-    ctx.fillStyle = "#000";
-    ctx.beginPath();
-
-    // draw grid
-    let posX = 0;
-    while (posX < width) {
-        let posY = 0;
-        while (posY < height) {
-            ctx.fillRect(posX, posY, 1, 1);
-            posY += gridSpace;
-        }
-        posX += gridSpace;
-        // if (posX < width) {
-        //     ctx.moveTo(posX, 0);
-        //     ctx.lineTo(posX, height);
-        //     ctx.stroke();
-        //     posX += gridSpace;
-        // }
-        // if (posY < height) {
-        //     ctx.moveTo(0, posY);
-        //     ctx.lineTo(width, posY);
-        //     ctx.stroke();
-        //     posY += gridSpace;
-        // }
-    }
-    ctx.restore();
-}
+    context.stroke();
+    context.restore();
+};
 
 export function getNormalizedZoom(zoom: number): NormalizedZoomValue {
     return Math.max(MIN_ZOOM, Math.min(zoom, 30)) as NormalizedZoomValue;
@@ -80,8 +73,19 @@ export function getNormalizedZoom(zoom: number): NormalizedZoomValue {
 
 function wheelHandler(
     e: WheelEvent,
-    setOrigin: (o: (v: Point) => Partial<Point>) => void,
-    setZoom: (o: (v: number) => NormalizedZoomValue) => void
+    setScroll: (
+        o: (
+            v: Point,
+            a: CanvasProperties
+        ) => Partial<CanvasProperties["scroll"]>
+    ) => void,
+    setZoom: (
+        o: (
+            v: number,
+            a: CanvasProperties
+        ) => Partial<Pick<CanvasProperties, "zoom" | "scroll">>
+    ) => void,
+    lastViewportPosition: Point
 ) {
     // need to throttle this
     e.preventDefault();
@@ -97,7 +101,7 @@ function wheelHandler(
             delta = MAX_STEP * sign;
         }
 
-        setZoom((v) => {
+        setZoom((v, canvasProperties) => {
             const zoom = { value: v };
             let newZoom = zoom.value - delta / 100;
             // increase zoom steps the more zoomed-in we are (applies to >100% only)
@@ -106,20 +110,27 @@ function wheelHandler(
                 -sign *
                 // reduced amplification for small deltas (small movements on a trackpad)
                 Math.min(1, absDelta / 20);
-            return getNormalizedZoom(newZoom);
+            return getStateForZoom(
+                {
+                    viewportX: lastViewportPosition.x,
+                    viewportY: lastViewportPosition.y,
+                    nextZoom: getNormalizedZoom(newZoom),
+                },
+                canvasProperties
+            );
         });
     }
 
     if (!isZoom) {
         const lockX = e.shiftKey && !e.metaKey;
         if (lockX) {
-            setOrigin((v) => ({
-                x: v.x + deltaX * rate,
+            setScroll((v, a) => ({
+                x: v.x - (deltaX * rate) / a.zoom,
             }));
         } else {
-            setOrigin((v) => ({
-                x: v.x + deltaX * rate,
-                y: v.y + deltaY * rate,
+            setScroll((v, a) => ({
+                x: v.x - (deltaX * rate) / a.zoom,
+                y: v.y - (deltaY * rate) / a.zoom,
             }));
         }
     }
@@ -128,7 +139,7 @@ function wheelHandler(
 type CanvasProperties = {
     width: number;
     height: number;
-    origin: Point;
+    scroll: Point;
     zoom: NormalizedZoomValue;
 };
 function Home() {
@@ -136,79 +147,46 @@ function Home() {
     const [canvasProperties, setCanvasProperties] = useState<CanvasProperties>({
         width: window.innerWidth || 300,
         height: window.innerHeight || 150,
-        origin: {
-            x: 0,
-            y: 0,
-        },
+        scroll: { x: 0, y: 0 },
         zoom: 1 as NormalizedZoomValue,
     });
 
     let canvasRef = useRef<HTMLCanvasElement>(null);
+    let lastViewportPosition = useRef<Point>({ x: 0, y: 0 });
 
-    const setOrigin = (o: (v: Point) => Partial<Point>) => {
+    const setScroll = (
+        o: (
+            v: Point,
+            a: CanvasProperties
+        ) => Partial<CanvasProperties["scroll"]>
+    ) => {
         setCanvasProperties((v) => ({
             ...v,
-            origin: {
-                ...v.origin,
-                ...o(v.origin),
+            scroll: {
+                ...v.scroll,
+                ...o(v.scroll, v),
             },
         }));
     };
 
-    const setZoom = (o: (v: number) => NormalizedZoomValue) => {
+    const setZoom = (
+        o: (
+            v: number,
+            a: CanvasProperties
+        ) => Partial<Pick<CanvasProperties, "zoom" | "scroll">>
+    ) => {
         setCanvasProperties((v) => ({
             ...v,
-            zoom: o(v.zoom),
+            ...o(v.zoom, v),
         }));
     };
-
-    const draw = useCallback(() => {
-        const ctx = canvasRef.current?.getContext("2d");
-        if (ctx && canvasRef.current) {
-            const { origin, zoom, width, height } = canvasProperties;
-            ctx.setTransform(1, 0, 0, 1, 0, 0);
-            ctx.save();
-            console.log("origin zoom: ", origin, zoom);
-
-            // clear background
-            ctx.save();
-            ctx.clearRect(0, 0, width, height);
-            ctx.restore();
-
-            ctx.setTransform(1, 0, 0, 1, origin.x, origin.y);
-            ctx.scale(zoom, zoom);
-
-            drawGrids(ctx, canvasProperties, { gridSpace });
-
-            // Some test element in the canvas
-            ctx.save();
-            ctx.strokeStyle = "#000";
-            ctx.strokeRect(0, 0, 100, 100);
-
-            ctx.strokeStyle = "#000";
-            ctx.strokeRect(60, 20, 20, 20);
-            ctx.strokeRect(20, 20, 20, 20);
-            ctx.strokeRect(20, 55, 60, 20);
-
-            ctx.strokeStyle = "#000";
-            ctx.strokeRect(0, 0, 2, 2);
-            ctx.restore();
-
-            ctx.restore();
-        }
-    }, [canvasProperties, gridSpace]);
-
-    // Sync canvas
-    useEffect(() => {
-        draw();
-    }, [draw]);
 
     // add event handlers
     useEffect(() => {
         let canvas = canvasRef.current;
 
         const _wheelHandler = (e: WheelEvent) =>
-            wheelHandler(e, setOrigin, setZoom);
+            wheelHandler(e, setScroll, setZoom, lastViewportPosition.current);
 
         if (canvas) {
             canvas.addEventListener("wheel", _wheelHandler, {
@@ -229,11 +207,73 @@ function Home() {
                 height: window.innerHeight,
             }));
         };
+        const _mouseMoveHanlder = (e: MouseEvent) => {
+            const { clientX, clientY } = e;
+            lastViewportPosition.current = { x: clientX, y: clientY };
+        };
         window.addEventListener("resize", _resizeHandler);
+        document.addEventListener("mousemove", _mouseMoveHanlder);
         return () => {
             window.removeEventListener("resize", _resizeHandler);
+            document.removeEventListener("mousemove", _mouseMoveHanlder);
         };
     }, []);
+
+    const draw = useCallback(() => {
+        const ctx = canvasRef.current?.getContext("2d");
+        if (ctx && canvasRef.current) {
+            const { scroll, zoom, width, height } = canvasProperties;
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+            ctx.save();
+            console.log("origin zoom: ", scroll, zoom);
+
+            // clear background
+            ctx.save();
+            ctx.clearRect(0, 0, width, height);
+            ctx.restore();
+
+            // apply zoom
+            ctx.scale(zoom, zoom);
+
+            strokeGrid(
+                ctx,
+                gridSpace,
+
+                -Math.ceil(zoom / gridSpace) * gridSpace +
+                (scroll.x % gridSpace),
+                -Math.ceil(zoom / gridSpace) * gridSpace +
+                (scroll.y % gridSpace),
+
+                // 0,0,
+
+                width / zoom,
+                height / zoom
+            );
+
+            // Some test element in the canvas
+            ctx.save();
+            ctx.translate(0 + scroll.x, 0 + scroll.y);
+            ctx.strokeStyle = "#000";
+            ctx.strokeRect(0, 0, 100, 100);
+
+            ctx.strokeStyle = "#000";
+            ctx.strokeRect(60, 20, 20, 20);
+            ctx.strokeRect(20, 20, 20, 20);
+            ctx.strokeRect(20, 55, 60, 20);
+
+            ctx.strokeStyle = "#000";
+            ctx.strokeRect(0, 0, 2, 2);
+            ctx.strokeRect(scroll.x, scroll.y, 2, 2);
+            ctx.restore();
+
+            ctx.restore();
+        }
+    }, [canvasProperties, gridSpace]);
+
+    // Sync canvas
+    useEffect(() => {
+        draw();
+    }, [draw]);
 
     const handleCanvasContextMenu: React.MouseEventHandler<
         HTMLCanvasElement
@@ -260,14 +300,18 @@ function Home() {
                 <div>
                     <button
                         onClick={() => {
-                            setZoom(() => getNormalizedZoom(0.75));
+                            setZoom(() => ({
+                                zoom: getNormalizedZoom(0.75),
+                            }));
                         }}
                     >
                         |zoom .75|
                     </button>
                     <button
                         onClick={() => {
-                            setZoom(() => getNormalizedZoom(1.5));
+                            setZoom(() => ({
+                                zoom: getNormalizedZoom(1.5),
+                            }));
                         }}
                     >
                         |zoom 1.5|
